8571f05a90d7a113c0c8ced35fc56b6b1994f63e
Ledyyy <133175634+Ledyyy@users.noreply.github.com>
2023-05-24 19:14:14 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,125 @@
+
+
+
+function setup() {
+  createCanvas(1000, 400);
+  //Crea los sprites necesarios para formar tu nombre
+  var a1;
+  var a2;
+  var a3;
+  var a4;
+  var a5;
+  var a6;
+  var a7;
+  var a8;
+  var a9;
+  var a10;
+  var a11;
+  var a12;
+  var a13;
+  var a14;
+  var a15;
+  var a16;
+  var a17;
+  var a18;
+  var a19;
+  var a20;
+  var a21;
+  var a22;
+  var a23;
+  var a24;
+  var a25;
+  var a26;
+  var a27;
+  var a28;
+  var a29;
+  var a30;
+  var a31;
+  var a32;
+  var a33;
+  var a34;
+  var a35;
+  var a36;
+ 
+a1=createSprite(200,140,10,10);
+a2=createSprite(200,130,10,10);
+a3=createSprite(200,120,10,10);
+a4=createSprite(200,110,10,10);
+a5=createSprite(210,110,10,10);
+a6=createSprite(220,110,10,10);
+a7=createSprite(220,140,10,10);
+a8=createSprite(220,130,10,10);
+a9=createSprite(220,120,10,10);
+a10=createSprite(220,110,10,10);
+a11=createSprite(210,130,10,10);
+a12=createSprite(240,140,10,10);
+a13=createSprite(240,130,10,10);
+a14=createSprite(240,120,10,10);
+a15=createSprite(240,110,10,10);
+a16=createSprite(250,140,10,10);
+a17=createSprite(260,140,10,10);
+a18=createSprite(280,140,10,15);
+a19=createSprite(280,130,10,10);
+a20=createSprite(280,120,10,10);
+a21=createSprite(280,110,10,10);
+a22=createSprite(290,145,10,5);
+a23=createSprite(300,145,10,5);
+a24=createSprite(290,125,10,5);
+a25=createSprite(300,125,10,5);
+a26=createSprite(290,105,10,5);
+a27=createSprite(300,105,10,5);
+a28=createSprite(320,140,10,10);
+a29=createSprite(330,130,10,10);
+a30=createSprite(320,120,10,10);
+a31=createSprite(310,110,10,10);
+a32=createSprite(310,150,10,10);
+a33=createSprite(340,120,10,10);
+a34=createSprite(350,110,10,10);
+a35=createSprite(350,150,10,10);
+a36=createSprite(340,140,10,10);
+a1.shapeColor= "WHITE"
+a2.shapeColor= "WHITE"
+a3.shapeColor= "WHITE"
+a4.shapeColor= "WHITE"
+a5.shapeColor= "WHITE"
+a6.shapeColor= "WHITE"
+a7.shapeColor= "WHITE"
+a8.shapeColor= "WHITE"
+a9.shapeColor= "WHITE"
+a10.shapeColor= "WHITE"
+a11.shapeColor= "WHITE"
+a12.shapeColor= "WHITE"
+a13.shapeColor= "WHITE"
+a14.shapeColor= "WHITE"
+a15.shapeColor= "WHITE"
+a16.shapeColor= "WHITE"
+a17.shapeColor= "WHITE"
+a18.shapeColor= "WHITE"
+a19.shapeColor= "WHITE"
+a20.shapeColor= "WHITE"
+a21.shapeColor= "WHITE"
+a22.shapeColor= "WHITE"
+a23.shapeColor= "WHITE"
+a24.shapeColor= "WHITE"
+a25.shapeColor= "WHITE"
+a26.shapeColor= "WHITE"
+a27.shapeColor= "WHITE"
+a28.shapeColor= "WHITE"
+a29.shapeColor= "WHITE"
+a30.shapeColor= "WHITE"
+a31.shapeColor= "WHITE"
+a32.shapeColor= "WHITE"
+a33.shapeColor= "WHITE"
+a34.shapeColor= "WHITE"
+a35.shapeColor= "WHITE"
+a36.shapeColor= "WHITE"
+
+}
+
+
+function draw() {
+  background("black");
+
+  drawSprites();
+
+}
